becc32f9f4303b4222ca716457923769578b7701
Qkessler <qkessler@gmail.com>
2023-11-05 17:14:56 +0100

    Add README with implementation notes and parse transactions

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use clap::Parser;
+use ledger_parser::{LedgerItem, Transaction};
+
+use futures::stream::TryStreamExt;
+use tokio::fs::File;
+use tokio::io::BufReader;
+use tokio_util::codec::{FramedRead, LinesCodec};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Transactions file path(s).
+    #[clap(short, long, required = true, num_args = 1..)]
+    files: Vec<String>,
+}
+
+async fn collect_transactions(
+    transaction: &str,
+) -> Result<Vec<Transaction>, Box<dyn std::error::Error>> {
+    Ok(ledger_parser::parse(transaction)?
+        .items
+        .into_iter()
+        .map(|item| match item {
+            LedgerItem::Transaction(transaction) => transaction,
+            _ => todo!(),
+        })
+        .collect())
+}
+
+async fn parse_transactions(path: &str) -> Result<Vec<Transaction>, Box<dyn std::error::Error>> {
+    let file = File::open(path).await?;
+    let reader = BufReader::new(file);
+    let mut transactions: Vec<Transaction> = Vec::new();
+    let mut lines = FramedRead::new(reader, LinesCodec::new());
+    let mut current_transaction = String::new();
+    let mut separator_count = 0;
+
+    while let Some(line) = lines.try_next().await? {
+        if line.is_empty() {
+            separator_count += 1;
+            if separator_count == 2 {
+                transactions.extend_from_slice(&collect_transactions(&current_transaction).await?);
+                current_transaction.clear();
+                separator_count = 0;
+            }
+        } else {
+            // Append non-empty lines to the current transaction
+            if !current_transaction.is_empty() {
+                current_transaction.push('\n');
+            }
+            current_transaction.push_str(&line);
+        }
+    }
+
+    if !current_transaction.is_empty() {
+        transactions.extend_from_slice(&collect_transactions(&current_transaction).await?);
+    }
+
+    Ok(transactions)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    println!("{:?}", args.files);
+    parse_transactions(&args.files[0]).await?;
+    Ok(())
+}
